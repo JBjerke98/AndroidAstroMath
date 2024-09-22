5e455d1dbe19240ac7c90b70aca0269bb60a9306
JBjerke98 <117266506+JBjerke98@users.noreply.github.com>
2024-09-22 11:45:51 +0200

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,13 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.0")
+
+    // JUnit for Unit testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // JUnit for AndroidX Instrumentation Tests
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    // Espresso for UI Testing
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
